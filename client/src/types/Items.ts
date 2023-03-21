2f158707b2f403ec4d7ba9c0b1fe44b3e636fc8a
Kevin Mok <south.kay@gmail.com>
2023-03-21 23:49:34 +1100

    Add types and types index

---
+++ client/src/types/Items.ts
@@ -0,0 +1,11 @@
+export interface Item {
+  examine: string
+  id: number
+  members: boolean
+  lowalch?: number
+  limit?: number
+  value: number
+  highalch?: number
+  icon: string
+  name: string
+}

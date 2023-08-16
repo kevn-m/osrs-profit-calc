4411bb9ddb39311aca9592aad11ede7d6137799e
Kevin Mok <south.kay@gmail.com>
2023-08-17 00:27:59 +1000

    Add error handling

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const cors = require("cors")
 
 app.use(express.json())
 app.use(cors())
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send("Something went wrong!")
+})
 
 require("dotenv").config()
 mongoose.connect(
@@ -19,8 +23,10 @@ const apiRouter = require("./routes/Api")
 app.use("/users", userRouter)
 app.use("/api", apiRouter)
 
-app.listen(3001, () => {
-  console.log("Server running on 3001...")
-})
+if (require.main === module) {
+  app.listen(3001, () => {
+    console.log("Server running on 3001...")
+  })
+}
 
 module.exports = app

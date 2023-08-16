c763b20b081fbd33365be948809b48c280ae5f45
Kevin Mok <south.kay@gmail.com>
2023-08-17 00:15:59 +1000

    Add mongo db back in

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ const cors = require("cors")
 app.use(express.json())
 app.use(cors())
 
-// require("dotenv").config()
-// mongoose.connect(
-//   `mongodb+srv://kevn-m:${process.env.MONGO_DB_PASSWORD}@cluster0.jy9unwn.mongodb.net/osrs-profit-calc?retryWrites=true&w=majority`
-// )
+require("dotenv").config()
+mongoose.connect(
+  `mongodb+srv://kevn-m:${process.env.MONGO_DB_PASSWORD}@cluster0.jy9unwn.mongodb.net/osrs-profit-calc?retryWrites=true&w=majority`
+)
 
 const userRouter = require("./routes/Users")
 const apiRouter = require("./routes/Api")

24b7d60ccb85af0736c189f9f402b09d739f7a61
Kevin Mok <south.kay@gmail.com>
2023-08-17 00:35:19 +1000

    Remove users routes

--- server/server.js
+++ server/server.js
@@ -17,10 +17,10 @@ mongoose.connect(
   `mongodb+srv://kevn-m:${process.env.MONGO_DB_PASSWORD}@cluster0.jy9unwn.mongodb.net/osrs-profit-calc?retryWrites=true&w=majority`
 )
 
-const userRouter = require("./routes/Users")
+// const userRouter = require("./routes/Users")
 const apiRouter = require("./routes/Api")
 
-app.use("/users", userRouter)
+// app.use("/users", userRouter)
 app.use("/api", apiRouter)
 
 if (require.main === module) {

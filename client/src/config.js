4974e52cfab459ea2b29a757946cf64602dbaf77
Kevin Mok <south.kay@gmail.com>
2023-08-17 01:01:49 +1000

    Small change ot URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === "production"
 
 export const apiUrl = isProduction
-  ? "https://osrs-item-prices-backend-lrttjn986-kevn-m.vercel.app/"
+  ? "https://osrs-item-prices-backend-lrttjn986-kevn-m.vercel.app"
   : "http://localhost:3001"

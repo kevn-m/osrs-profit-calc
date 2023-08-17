15836b34506fb9a014f0c6936dd1e18c972b6c2f
Kevin Mok <south.kay@gmail.com>
2023-08-17 21:24:16 +1000

    Add logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
 import React from "react"
 import { HomePage } from "./pages/HomePage"
+import { Logo } from "./components/Logo"
 
 export const App = () => {
-  return <HomePage />
+  return (
+    <div className="bg-slate-300 h-screen">
+      <Logo />
+      <HomePage />
+    </div>
+  )
 }
---
+++ client/src/components/Logo.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import osrsItemLogo from "../assets/osrsItemLogo.png"
+
+export const Logo = () => {
+  return (
+    <div>
+      <img
+        src={osrsItemLogo}
+        alt="OSRS Item Logo"
+        className="mx-auto w-52 rounded-full pt-3"
+      />
+    </div>
+  )
+}

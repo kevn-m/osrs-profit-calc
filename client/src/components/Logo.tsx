ee42be08531d6737465bd2abe22073085ba7c931
Kevin Mok <south.kay@gmail.com>
2023-08-17 21:39:14 +1000

    Add new logo

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -7,7 +7,7 @@ export const Logo = () => {
       <img
         src={osrsItemLogo}
         alt="OSRS Item Logo"
-        className="mx-auto w-52 rounded-full pt-3"
+        className="mx-auto w-52 rounded-full pt-3 shadow-lg"
       />
     </div>
   )

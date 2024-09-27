5a871510fe2d4bc1eb78c63b97eb3ae925674530
Kevin Mok <south.kay@gmail.com>
2024-09-27 21:07:19 +1000

    Update route

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === "production"
 
 export const apiUrl = isProduction
-  ? "https://osrs-profit-calc.vercel.app/"
+  ? "https://osrs-profit-calc.vercel.app"
   : "http://localhost:3001"
